3a5ad4f03dcb30d0fe01c11c0e37f17d65fd6f7e
Michael Le <mnle529@gmail.com>
2020-04-19 16:23:47 -0700

    added saved page

---
+++ client/src/pages/Saved/Saved.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import SavedBooks from "../../components/SavedBooks/SavedBooks"
+import API from "../../utils/API";
+
+function Saved() {
+
+    const [savedBooks, setSavedBooks] = useState([]);
+
+    const handleDeleteButton = id => {
+        console.log("delete button clicked");
+
+        API.removeBook(id)
+            .then(res => {
+                console.log("book deleted");
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        API.getSaved()
+            .then(res => {
+                setSavedBooks(res.data);
+            });
+    }, [handleDeleteButton]);
+
+    return (
+        <>
+            <Header />
+            <SavedBooks books={savedBooks} handleDelete={handleDeleteButton} />
+        </>
+    );
+}
+
+export default Saved;
