ca4115885df76707f4f5d3374b5be44f18c38533
Michael Le <mnle529@gmail.com>
2020-04-19 15:06:44 -0700

    added search page

---
+++ client/src/pages/Search/Search.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import SearchForm from "../../components/SearchForm/SearchForm"
+import Results from "../../components/Results/Results";
+import useDebounce from "../../utils/debounceHook";
+import API from "../../utils/API";
+
+function Search() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(() => {
+        if (!searchTerm) {
+            return;
+        }
+        if (debouncedSearchTerm) {
+            API.searchBooks(searchTerm)
+                .then(res => {
+                    console.log(res);
+                    setSearchResults(res.data.map(book => {
+                        return {
+                            id: book.id,
+                            title: book.volumeInfo.title,
+                            authors: book.volumeInfo.authors,
+                            description: book.volumeInfo.description,
+                            imageURL: (book.volumeInfo.imageLinks) ? book.volumeInfo.imageLinks.thumbnail : "../../assets/images/no_image.png",
+                            link: book.volumeInfo.infoLink
+                        }
+                    }));
+                });
+        }
+    }, [debouncedSearchTerm]);
+
+    const handleSaveButton = param => {
+        const index = searchResults.filter(book => {
+            return book.id !== param;
+        });
+        const book = searchResults[index];
+
+        API.saveBook(book)
+            .then(res => {
+                const books = searchResults.filter(book => {
+                    return book.id !== param;
+                });
+                setSearchResults(books);
+            })
+            .catch(err => console.log(err));
+    };
+
+    const handleInputChange = event => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <Header />
+            <SearchForm searchTerm={searchTerm} onChange={handleInputChange} onClick={handleFormSubmit} />
+            <Results books={searchResults} handleSave={handleSaveButton} />
+        </>
+    )
+}
+
+export default Search;
