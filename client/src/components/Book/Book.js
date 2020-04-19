a50e451333a71e3351552e05eb445ce1ad26ded9
Michael Le <mnle529@gmail.com>
2020-04-19 02:38:16 -0700

    added api routes

---
+++ client/src/components/Book/Book.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Book(props) {
+    const book = props.book;
+
+    let style = {
+        marginRight: "15px"
+    }
+
+    return (
+        <div className="box">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-128x128 is-2by3">
+                        <img src={book.imageURL} alt={book.title + " cover"} />
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <h2 className="title">{book.title}</h2>
+                    <h3 className="subtitle">Written by: {book.authors}</h3>
+                    <div className="content">
+                        <p>{book.description}</p>
+                    </div>
+                    <div className="level is-mobile">
+                        <div className="level-left">
+                            <button className="button" style={styles} onClick={() => window.open(book.link)}>View</button>
+                            {props.children}
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    );
+}
+
+export default Book;
---
+++ controllers/searchController.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+module.exports = {
+    searchByQuery: function (req, res) {
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=" + req.params.q + "&key=" + process.env.API_KEY)
+            .then(booksData => {
+                res.json(booksData.data.items)
+            })
+            .catch(err => res.status(422).json(err));
+    }
+};
